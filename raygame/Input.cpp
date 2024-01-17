4f75dedf2b8fecfb9767a7ad408ab3b7240821b3
Ethan Gandy <gandyethan4@gmail.com>
2024-01-17 11:11:59 -0600

    Input move component

--- raygame/Input.cpp
+++ raygame/Input.cpp
@@ -2,6 +2,8 @@
 #include <Vector2.h>
 #include "raylib.h"
 #include "Input.h"
+#include "Actor.h"
+
 
 char Input::getInput()
 {
@@ -19,29 +21,28 @@ void Input::draw()
 
 void Input::update(float deltaTime)
 {
-
+	Actor::update(deltaTime);
 
 	MathLibrary::Vector2 direction = MathLibrary::Vector2(0,0);
 
-	if (IsKeyPressed(KEY_D))
+	if (IsKeyPressed(KEY_D) || IsKeyPressed(KEY_RIGHT))
 	{
-		
+		direction = direction + MathLibrary::Vector2(1, 0);
 	}
-	if (IsKeyPressed(KEY_A))
+	if (IsKeyPressed(KEY_A) || IsKeyPressed(KEY_LEFT))
 	{
-
 		direction = direction + MathLibrary::Vector2(-1, 0);
 	}
-	if (IsKeyPressed(KEY_W))
+	if (IsKeyPressed(KEY_W) || IsKeyPressed(KEY_UP))
 	{
-
+		direction = direction + MathLibrary::Vector2(0, -1);
 	}
-	if (IsKeyPressed(KEY_S))
+	if (IsKeyPressed(KEY_S) || IsKeyPressed(KEY_DOWN))
 	{
-
+		direction = direction + MathLibrary::Vector2(0, 1);
 	}
 
 	
-
+	
 
 }
